98fdbef97703f25cbb57522722aa3ed0ca2088ed
M Faris Alkodri <faris.alkodri@inphosoft.com>
2024-10-23 10:15:10 +0700

    - update package.json

--- index.d.ts
+++ index.d.ts
@@ -2,13 +2,15 @@ declare module 'html2pdf.js' {
   interface Html2PdfOptions {
     margin?: number | [number, number, number, number];
     filename?: string;
-    image?: { type: string; quality: number };
-    html2canvas?: { scale: number };
+    image?: { type?: string; quality?: number };
+    html2canvas?: { scale?: number, dpi?: number };
     jsPDF?: { unit: string; format: string; orientation: string };
   }
 
+  function set(options: Html2PdfOptions): any;
+
   export default function html2pdf(
-    element: HTMLElement | string,
+    element?: HTMLElement | string,
     options?: Html2PdfOptions
-  ): Promise<void>;
+  ): ({set: typeof set});
 }
